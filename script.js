0421d997feac3f74a015f93b9ea8312db2c0f41f
Rosmeiry Adames <rosmeiryadames421@gmail.com>
2017-03-20 14:20:40 +0000

    pentagon

--- script.js
+++ script.js
@@ -12,7 +12,28 @@ right(90);
 forward(50);
 }
 
+function triangle(){
+forward(75);
+right(120);
+forward(75);
+right(120);
+forward(75);
+}
+
+function pentagon(){
+    forward(100);
+    right(50);
+    forward(50);
+    right(70);
+    forward(50);
+    right(60);
+    forward(100);
+    right(90);
+    forward(70);
+}
+
+
+
 // Type your function call below
-
-
+pentagon();
 stamp();
